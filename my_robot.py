a628d2f335121dd7910d2d0c2fa28baa2e3fff5c
root <root@killingjacky.jetos.com>
2014-08-18 05:26:27 -0400

    adfadfadfadsf

--- my_robot.py
+++ my_robot.py
@@ -2,10 +2,15 @@ import werobot
 
 robot = werobot.WeRoBot(token='1234abcd')
 
-@robot.handler
+@robot.text
 def echo(message):
     print message.target
     print message.source
+    print message.content
     return 'Hello World!'
+
+@robot.handler
+def default_handler(message):
+    return 'Default Response'
 
 robot.run(server='waitress',port='8123')
